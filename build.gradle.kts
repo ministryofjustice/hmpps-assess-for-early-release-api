78caa26f8cfa2cbf68c23cc27f54fee1be219c91
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-07-31 11:34:38 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  kotlin("plugin.spring") version "2.0.0"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.8.0")
+}
+
+kotlin {
+  jvmToolchain(21)
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+  }
+}
