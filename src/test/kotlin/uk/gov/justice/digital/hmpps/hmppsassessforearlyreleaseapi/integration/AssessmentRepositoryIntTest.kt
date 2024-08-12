e60e026a920b2ebdbf26276f1cb83ce63f0a5dd5
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-08-12 13:40:36 +0100

    CVSL-2068: Fix assessment repository test.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/integration/AssessmentRepositoryIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/integration/AssessmentRepositoryIntTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.integration
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
@@ -17,7 +18,9 @@ class AssessmentRepositoryIntTest : IntegrationTestBase() {
     val assessment = Assessment(1, 1234L, "G4274GN")
     assessmentRepository.save(assessment)
 
-    val dbAssessment = assessmentRepository.findByIdOrNull(assessment.id)
-    assertThat(dbAssessment).isEqualTo(assessment)
+    val dbAssessment = assessmentRepository.findByIdOrNull(assessment.id) ?: fail("assessment with id ${assessment.id} not found")
+    assertThat(dbAssessment.id).isEqualTo(assessment.id)
+    assertThat(dbAssessment.prisonerNumber).isEqualTo(assessment.prisonerNumber)
+    assertThat(dbAssessment.bookingId).isEqualTo(assessment.bookingId)
   }
 }
