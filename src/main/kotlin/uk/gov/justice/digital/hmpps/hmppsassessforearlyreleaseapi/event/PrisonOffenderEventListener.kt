2359608cad2e449bfd3d3ca8bc46f7ba4129b042
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-08-28 10:26:36 +0100

    detekt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/event/PrisonOffenderEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/event/PrisonOffenderEventListener.kt
@@ -21,7 +21,7 @@ class PrisonOffenderEventListener(
   }
 
   @SqsListener("domaineventsqueue", factory = "hmppsQueueContainerFactoryProxy")
-  @WithSpan(value = "hmpps-assess-for-early-release-prisoner-event-queue", kind = SpanKind.SERVER)
+  @WithSpan(value = "hmpps-assess-for-early-release-prisoner-domain-event-queue", kind = SpanKind.SERVER)
   fun onPrisonOffenderEvent(requestJson: String) {
     val (message, messageAttributes) = mapper.readValue(requestJson, HMPPSMessage::class.java)
     val eventType = messageAttributes.eventType.Value
