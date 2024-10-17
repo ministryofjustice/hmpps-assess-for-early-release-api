5c2d6c170d856709bd2653921c2f6fd5d2ce7187
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-10-17 10:17:56 +0100

    CVSL-2176: Update gradle spring boot plugin to latest version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.5"
   kotlin("plugin.spring") version "2.0.10"
   kotlin("plugin.jpa") version "2.0.10"
   id("io.gitlab.arturbosch.detekt") version "1.23.6"
