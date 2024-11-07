96d04c937219ee05ee4e6f82a09f7c6eb88d0b5e
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-11-07 10:10:25 +0000

    CVSL-2254: Remove debug code.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/probation/ProbationSearchApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/probation/ProbationSearchApiClient.kt
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.service.typeReference
 
 @Component
@@ -13,19 +12,14 @@ class ProbationSearchApiClient(@Qualifier("oauthProbationSearchApiClient") val p
   fun searchForPersonOnProbation(
     nomisId: String,
   ): OffenderDetail? {
-    try {
-      val probationOffenderSearchResponse = probationSearchApiClient
-        .post()
-        .uri("/search")
-        .bodyValue(OffenderSearchRequest(nomsNumber = nomisId))
-        .accept(MediaType.APPLICATION_JSON)
-        .retrieve()
-        .bodyToMono(typeReference<List<OffenderDetail>>())
-        .block()
-      return if (probationOffenderSearchResponse?.isNotEmpty() == true) probationOffenderSearchResponse[0] else null
-    } catch (e: WebClientResponseException) {
-      println(e)
-      throw e
-    }
+    val probationOffenderSearchResponse = probationSearchApiClient
+      .post()
+      .uri("/search")
+      .bodyValue(OffenderSearchRequest(nomsNumber = nomisId))
+      .accept(MediaType.APPLICATION_JSON)
+      .retrieve()
+      .bodyToMono(typeReference<List<OffenderDetail>>())
+      .block()
+    return if (probationOffenderSearchResponse?.isNotEmpty() == true) probationOffenderSearchResponse[0] else null
   }
 }
