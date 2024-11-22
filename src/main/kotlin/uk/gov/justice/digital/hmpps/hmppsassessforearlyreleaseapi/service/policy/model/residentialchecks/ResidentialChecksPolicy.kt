268501036c89f82810de81e6f5c2e4bdb7e76ffe
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-11-22 16:08:29 +0000

    CVSL-2259: An initial version of the residential checks policy.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/policy/model/residentialchecks/ResidentialChecksPolicy.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.service.policy.model.residentialchecks
+
+enum class ResidentialChecksPolicyVersion {
+  V1,
+}
+
+enum class ResidentialChecksStatus {
+  NOT_STARTED,
+  IN_PROGRESS,
+  UNSUITABLE,
+  SUITABLE,
+}
+
+enum class TaskStatus {
+  NOT_STARTED,
+  IN_PROGRESS,
+  UNSUITABLE,
+  SUITABLE,
+}
+
+enum class InputType {
+  TEXT,
+  RADIO,
+  DATE,
+  ADDRESS,
+  CHECKBOX,
+}
+
+data class Option(
+  val value: String,
+)
+
+data class Input(
+  val type: InputType,
+  val options: List<Option>? = null,
+)
+
+data class Question(
+  val code: String,
+  val text: String,
+  val hintText: String? = null,
+  val input: Input,
+)
+
+data class Section(
+  val header: String? = null,
+  val hintText: String? = null,
+  val questions: List<Question>,
+)
+
+data class Task(
+  val code: String,
+  val name: String,
+  val sections: List<Section>,
+)
+
+data class ResidentialChecks(
+  val tasks: List<Task>,
+  val overallStatus: ResidentialChecksStatus,
+  val version: ResidentialChecksPolicyVersion,
+)
+
+data class ResidentialChecksPolicy(
+  val version: ResidentialChecksPolicyVersion,
+  val tasks: List<Task>,
+)
