f88706e04004314536b95dd034a2428f4fb02907
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-01-08 10:25:30 +0000

    CVSL-2400-AFER-Render-css-and-images-to-PDF-generation

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfServiceTest.kt
@@ -6,6 +6,7 @@ import org.mockito.Mockito.mock
 import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Value
 import org.thymeleaf.TemplateEngine
 
 class PdfServiceTest {
