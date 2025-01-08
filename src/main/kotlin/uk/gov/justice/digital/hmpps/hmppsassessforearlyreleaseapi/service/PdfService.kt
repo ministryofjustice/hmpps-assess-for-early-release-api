dfcb0e16795a7d145fc2a8c6b4aafff337186076
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2025-01-08 16:02:13 +0000

    Cvsl 2400 afer render css and images to pdf generation (#86)
    * CVSL-2211-AFER-Set-up-Gotenberg-for-pdf-generation
    
    * refactor
    
    * update gotenberg to 8
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation
    
    * CVSL-2400-AFER-Render-css-and-images-to-PDF-generation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -10,12 +11,16 @@ import org.thymeleaf.context.Context
 import java.nio.charset.StandardCharsets
 
 @Service
-class PdfService(private val templateEngine: TemplateEngine, private val gotenbergApiClient: GotenbergApiClient) {
-
+class PdfService(
+  private val templateEngine: TemplateEngine,
+  private val gotenbergApiClient: GotenbergApiClient,
+  @Value("\${assessments.url}") private val assessmentsUrl: String,
+) {
   fun generateHtml(title: String, message: String): String {
     val context = Context()
     context.setVariable("title", title)
     context.setVariable("message", message)
+    context.setVariable("assessmentsUrl", assessmentsUrl)
     return templateEngine.process("sample", context)
   }
 
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/PdfServiceTest.kt
@@ -12,18 +12,20 @@ class PdfServiceTest {
 
   private var templateEngine = mock<TemplateEngine>()
   private val gotenbergClient = mock<GotenbergApiClient>()
+  private val assessmentsUrl = "http://host.docker.internal:8089"
 
   private val service: PdfService =
     PdfService(
       templateEngine,
       gotenbergClient,
+      assessmentsUrl,
     )
 
   @Test
   fun `generatePdf should return PDF bytes`() {
     val title = "Test Title"
     val message = "Test Message"
-    val htmlContent = "<html><body><h1>$title</h1><p>$message</p></body></html>"
+    val htmlContent = "<html><head><link rel=\"stylesheet\" th:href=\"$assessmentsUrl + '/css/style.css'\"></head><body><h1>$title</h1><p>$message</p></body></html>"
 
     whenever(templateEngine.process(eq("sample"), any())).thenReturn(htmlContent)
     whenever(gotenbergClient.convertHtmlToPdf(any())).thenReturn("PDF_BYTES".toByteArray())
