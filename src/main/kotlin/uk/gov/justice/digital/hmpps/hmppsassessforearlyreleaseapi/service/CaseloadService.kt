b51be8cc1c23ab601171e4b4cf1a609bf542fd95
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-04-02 12:18:31 +0100

    CVSL-2689-AFER-Render-HDC-Application-Approved-Form

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/CaseloadService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/service/CaseloadService.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.service
 
 import jakarta.transaction.Transactional
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.Assessment
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.model.OffenderSummaryResponse
@@ -17,17 +19,22 @@ class CaseloadService(
 
   companion object {
     const val DAYS_BEFORE_SENTENCE_START = 10L
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
   @Transactional
   fun getCaseAdminCaseload(prisonCode: String): List<OffenderSummaryResponse> {
+    log.info("Fetching caseload for staffCode: $prisonCode")
     val assessments = assessmentRepository.findByOffenderPrisonIdAndDeletedTimestampIsNull(prisonCode)
+    log.info("assessments: $assessments")
     return assessments.map { createOffenderSummary(it) }
   }
 
   @Transactional
   fun getComCaseload(staffCode: String): List<OffenderSummaryResponse> {
+    log.info("Fetching caseload for staffCode: $staffCode")
     val assessments = assessmentRepository.findByResponsibleComStaffCodeAndDeletedTimestampIsNull(staffCode)
+    log.info("assessments: $assessments")
     return assessments.map { createOffenderSummary(it) }
   }
 
