087263177d0505998c9aea71d054248eadfd9409
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-04-09 10:16:01 +0100

    CVSL-2748: Dependency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.4"
-  kotlin("plugin.spring") version "2.1.10"
-  kotlin("plugin.jpa") version "2.1.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.0.0"
+  kotlin("plugin.spring") version "2.1.20"
+  kotlin("plugin.jpa") version "2.1.20"
   id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
@@ -21,12 +21,12 @@ ext["hibernate.version"] = "6.5.3.Final"
 ext["logback.version"] = "1.5.14"
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0")
-  implementation("org.springframework.security:spring-security-config:6.4.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.2")
+  implementation("org.springframework.security:spring-security-config:6.4.4")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.13.1")
-  implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.2")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.14.0")
+  implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.9")
   implementation("com.tinder.statemachine:statemachine:0.2.0")
 
   // Database dependencies
@@ -34,23 +34,23 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.7.5")
 
   // SQS/SNS dependencies
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.2")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
   // Thymeleaf
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.3.0")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
   testImplementation("com.h2database:h2")
-  testImplementation("org.testcontainers:postgresql:1.20.5")
-  testImplementation("org.testcontainers:localstack:1.20.5")
+  testImplementation("org.testcontainers:postgresql:1.20.6")
+  testImplementation("org.testcontainers:localstack:1.20.6")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("io.projectreactor:reactor-test")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.26") {
     exclude(group = "io.swagger.core.v3")
   }
 }
