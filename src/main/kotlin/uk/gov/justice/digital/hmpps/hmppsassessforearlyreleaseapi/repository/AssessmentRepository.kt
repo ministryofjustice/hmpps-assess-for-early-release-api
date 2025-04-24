8d80f6a77eba3a3e4aade11b06933c1e375dfd7c
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2025-04-24 14:55:47 +0100

    CVSL-2779: Use a join on offender when fetching a potentially large number of assessments instead of selecting each offender for each assessment. (#180)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/repository/AssessmentRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/repository/AssessmentRepository.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.Assessment
@@ -11,13 +12,18 @@ interface AssessmentRepository : JpaRepository<Assessment, Long> {
 
   fun findByOffenderPrisonNumberAndDeletedTimestampIsNullOrderByCreatedTimestamp(prisonNumber: String): List<Assessment>
 
+  @EntityGraph(value = "Assessment.offender")
   fun findByResponsibleComStaffCodeAndStatusInAndDeletedTimestampIsNull(staffCode: String, status: List<AssessmentStatus>): List<Assessment>
 
+  @EntityGraph(value = "Assessment.offender")
   fun findByOffenderPrisonIdAndDeletedTimestampIsNull(prisonId: String): List<Assessment>
 
+  @EntityGraph(value = "Assessment.offender")
   fun findByOffenderPrisonNumberOrderById(prisonId: String): List<AssessmentSearchResponse>
 
+  @EntityGraph(value = "Assessment.offender")
   fun findByResponsibleComStaffCodeAndDeletedTimestampIsNull(staffCode: String): List<Assessment>
 
+  @EntityGraph(value = "Assessment.offender")
   fun findByTeamCodeInAndDeletedTimestampIsNull(teamCodes: List<String>): List<Assessment>
 }
