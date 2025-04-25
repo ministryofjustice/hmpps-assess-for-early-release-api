1ff0c7ee88d183943f5e464aa2b73b20dbc170ef
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-04-25 13:43:51 +0100

    CVSL-2743-AfER-PP-tasks-and-handover-rules

--- src/main/resources/migration/common/V56__add_address_check_status.sql
+++ src/main/resources/migration/common/V56__add_address_check_status.sql
@@ -1,2 +1,2 @@
 ALTER TABLE assessment ADD COLUMN address_checks_status VARCHAR(20) NOT NULL DEFAULT 'NOT_STARTED'
-    CONSTRAINT address_checks_status_chk CHECK(status in ('NOT_STARTED'));
+    CONSTRAINT address_checks_status_chk CHECK(address_checks_status in ('NOT_STARTED'));
