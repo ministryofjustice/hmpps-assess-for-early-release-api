52051b1dab37c167ec0949127ae5fd1a148f5ac7
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-05-01 09:09:45 +0100

    CVSL-2819 Being more explicit around tasks not being available

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/entity/state/Task.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessforearlyreleaseapi/entity/state/Task.kt
@@ -21,6 +21,7 @@ import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.state.T
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.state.TaskStatus.IN_PROGRESS
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.state.TaskStatus.LOCKED
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.state.TaskStatus.READY_TO_START
+import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.entity.state.TaskStatus.UNAVAILABLE
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.model.EligibilityStatus
 import uk.gov.justice.digital.hmpps.hmppsassessforearlyreleaseapi.service.policy.model.residentialchecks.ResidentialChecksStatus
 
@@ -28,7 +29,8 @@ enum class Task {
 
   ASSESS_ELIGIBILITY {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.eligibilityChecksStatus) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.eligibilityChecksStatus) {
       EligibilityStatus.NOT_STARTED -> READY_TO_START
       EligibilityStatus.IN_PROGRESS -> IN_PROGRESS
       else -> COMPLETE
@@ -37,137 +39,147 @@ enum class Task {
 
   ENTER_CURFEW_ADDRESS {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       NOT_STARTED, ELIGIBILITY_AND_SUITABILITY_IN_PROGRESS, INELIGIBLE_OR_UNSUITABLE -> LOCKED
       ELIGIBLE_AND_SUITABLE -> READY_TO_START
       AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS -> COMPLETE
       OPTED_OUT -> IN_PROGRESS
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   REVIEW_APPLICATION_AND_SEND_FOR_DECISION {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       NOT_STARTED, ELIGIBILITY_AND_SUITABILITY_IN_PROGRESS, ELIGIBLE_AND_SUITABLE, AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS, INELIGIBLE_OR_UNSUITABLE, OPTED_OUT -> LOCKED
       AWAITING_PRE_DECISION_CHECKS -> READY_TO_START
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   COMPLETE_14_DAY_CHECKS {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       NOT_STARTED, ELIGIBILITY_AND_SUITABILITY_IN_PROGRESS, ELIGIBLE_AND_SUITABLE, AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS, INELIGIBLE_OR_UNSUITABLE, OPTED_OUT -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   COMPLETE_2_DAY_CHECKS {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       NOT_STARTED, ELIGIBILITY_AND_SUITABILITY_IN_PROGRESS, ELIGIBLE_AND_SUITABLE, AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS, INELIGIBLE_OR_UNSUITABLE, OPTED_OUT -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   PRINT_LICENCE {
     override fun visibleForRoles() = setOf(PRISON_CA)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       NOT_STARTED, ELIGIBILITY_AND_SUITABILITY_IN_PROGRESS, ELIGIBLE_AND_SUITABLE, AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS, INELIGIBLE_OR_UNSUITABLE, OPTED_OUT -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   CONSULT_THE_VLO_AND_POM {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_ADDRESS_AND_RISK_CHECKS ->
         if (assessment.victimContactSchemeOptedIn != null) COMPLETE else READY_TO_START
 
       ADDRESS_AND_RISK_CHECKS_IN_PROGRESS -> COMPLETE
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   CHECK_ADDRESSES_OR_COMMUNITY_ACCOMMODATION {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_ADDRESS_AND_RISK_CHECKS -> READY_TO_START
       ADDRESS_AND_RISK_CHECKS_IN_PROGRESS -> IN_PROGRESS
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   RECORD_NON_DISCLOSABLE_INFORMATION {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS ->
         if (assessment.hasNonDisclosableInformation != null) COMPLETE else READY_TO_START
 
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   MAKE_A_RISK_MANAGEMENT_DECISION {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   SEND_CHECKS_TO_PRISON {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? {
-      if (!assessment.status.isOneOf(AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS)) {
-        return null
-      }
-      return when {
-        assessment.addressChecksStatus == ResidentialChecksStatus.UNSUITABLE -> READY_TO_START
-        assessment.addressChecksStatus == ResidentialChecksStatus.SUITABLE && assessment.victimContactSchemeOptedIn != null && assessment.hasNonDisclosableInformation != null -> READY_TO_START
-        else -> LOCKED
-      }
+
+    override fun taskStatus(assessment: Assessment) = when {
+      !assessment.status.isOneOf(AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS) -> UNAVAILABLE
+      assessment.addressChecksStatus == ResidentialChecksStatus.UNSUITABLE -> READY_TO_START
+      assessment.addressChecksStatus == ResidentialChecksStatus.SUITABLE && assessment.victimContactSchemeOptedIn != null && assessment.hasNonDisclosableInformation != null -> READY_TO_START
+      else -> LOCKED
     }
   },
 
   CREATE_LICENCE {
     override fun visibleForRoles() = setOf(PROBATION_COM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_ADDRESS_AND_RISK_CHECKS, ADDRESS_AND_RISK_CHECKS_IN_PROGRESS -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   CONFIRM_RELEASE {
     override fun visibleForRoles() = setOf(PRISON_DM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_DECISION, AWAITING_REFUSAL -> READY_TO_START
       TIMED_OUT, OPTED_OUT -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   APPROVE_LICENCE {
     override fun visibleForRoles() = setOf(PRISON_DM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       AWAITING_DECISION, AWAITING_REFUSAL, TIMED_OUT, OPTED_OUT -> LOCKED
       APPROVED -> READY_TO_START
-      else -> null
+      else -> UNAVAILABLE
     }
   },
 
   OPT_IN {
     override fun visibleForRoles() = setOf(PRISON_DM)
-    override fun taskStatus(assessment: Assessment): TaskStatus? = when (assessment.status) {
+
+    override fun taskStatus(assessment: Assessment) = when (assessment.status) {
       OPTED_OUT -> LOCKED
-      else -> null
+      else -> UNAVAILABLE
     }
   },
   ;
 
   open fun visibleForRoles() = emptySet<UserRole>()
-  abstract fun taskStatus(assessment: Assessment): TaskStatus?
+  abstract fun taskStatus(assessment: Assessment): TaskStatus
 
   companion object {
     val TASKS_BY_ROLE = Task.entries
@@ -177,6 +189,7 @@ enum class Task {
 }
 
 enum class TaskStatus {
+  UNAVAILABLE,
   LOCKED,
   READY_TO_START,
   IN_PROGRESS,
