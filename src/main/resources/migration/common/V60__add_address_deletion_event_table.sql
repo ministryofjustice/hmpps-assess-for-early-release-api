8a722922fb43390fe2f4acf769ae40350a1632c3
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-05-06 13:25:47 +0100

    CVSL-2764-AFER-Soft-delete-address-check-requests

--- src/main/resources/migration/common/V60__add_address_deletion_event_table.sql
+++ src/main/resources/migration/common/V60__add_address_deletion_event_table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE address_deletion_event
 (
     id SERIAL NOT NULL CONSTRAINT address_deletion_event_pk PRIMARY KEY,
-    address_delete_reason_type VARCHAR(40)
-        CONSTRAINT address_delete_reason_type_chk CHECK
+    reason_type VARCHAR(40)
+        CONSTRAINT reason_type_chk CHECK
             (
-                address_delete_reason_type IN (
+                reason_type IN (
                                                'NO_LONGER_WANTS_TO_BE_RELEASED_HERE',
                                                'NOT_ENOUGH_TIME_TO_ASSESS',
                                                'HAS_ANOTHER_SUITABLE_ADDRESS',
                                                'OTHER_REASON'
                 )
             ),
-    address_delete_other_reason VARCHAR(255),
+    other_reason VARCHAR(255),
     assessment_event_id BIGINT NULL,
     CONSTRAINT fk_address_deletion_event FOREIGN KEY (assessment_event_id) REFERENCES assessment_event(id)
 );
